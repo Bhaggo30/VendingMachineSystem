6fea9f393385e000663e98a47a4caa416f01433f
bhagyashree <bhaggo30july@gmail.com>
2024-09-10 13:47:50 +0530

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,56 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws VendingMachineException {
+        VendingMachine vendingMachine = new VendingMachine();
+        Scanner sc = new Scanner(System.in);
+
+        // Adding some products to the vending machine
+        vendingMachine.addProduct(new Product("Soda", 1.50, 10));
+        vendingMachine.addProduct(new Product("Chips", 1.00, 5));
+        vendingMachine.addProduct(new Product("Candy", 0.75, 20));
+
+        while (true) {
+            System.out.println("\nVending Machine Menu:");
+            System.out.println("1. Display Products");
+            System.out.println("2. Buy Product");
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice:");
+
+            int choice = sc.nextInt();
+            sc.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    vendingMachine.displayProducts();
+                    break;
+
+                case 2:
+                    System.out.println("Enter the product name:");
+                    String productName = sc.nextLine();
+
+                    try {
+                        System.out.println("Enter amount inserted (in dollars):");
+                        double amountInserted = sc.nextDouble();
+                        sc.nextLine(); // Consume newline
+                        vendingMachine.buyProduct(productName, amountInserted);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting Vending Machine.");
+                    sc.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice, please try again.");
+                    break;
+            }
+        }
+    }
+}
